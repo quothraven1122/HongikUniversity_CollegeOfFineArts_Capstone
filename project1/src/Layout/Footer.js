aa1020f124c2b1c56f447d6da6576c5245a34a75
hjee02018@naver.com <hjee02018@naver.com>
2023-08-06 17:37:29 +0900

    Add source file

---
+++ project1/src/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>footer이다.</p>
+    </div>
+  );
+};
+
+export default Footer;
