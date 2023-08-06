1c9ec64a5d561e2aea56077af8a9544dbd58d381
hjee02018@naver.com <hjee02018@naver.com>
2023-08-06 18:49:19 +0900

    Mod val initialization

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -16,15 +16,29 @@ function FuncComp(props) {
   var number = numberState[0];
   var setNumber = numberState[1];
 
+  // var dateState = useState(new Date().toString());
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+
+  var [_date, setDate] = useState(new Date().toString());
+
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>Number:{number}</p>
+      <p>Date : {_date}</p>
       <input
         type="button"
         value="random"
         onClick={function () {
           setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
         }}
       ></input>
     </div>
@@ -34,17 +48,26 @@ function FuncComp(props) {
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
+    date: new Date().toString(),
   };
   render() {
     return (
       <div className="container">
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={function () {
             this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
           }.bind(this)}
         ></input>
       </div>
